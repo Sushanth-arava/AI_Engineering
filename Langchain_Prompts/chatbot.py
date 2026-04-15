3b0b5ccd2fff1025f55f86e529319485a1c079d7
Sushanth Arava <aravasushanth77@gmail.com>
2026-04-15 11:09:46 -0400

    language prompts and structured outputs

--- Langchain_Prompts/chatbot.py
+++ Langchain_Prompts/chatbot.py
@@ -1,17 +1,19 @@
+from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
 from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
 
 load_dotenv()
 
 model = ChatOpenAI()
-chat_history = []
-while True:
-    user_input = input("You:  ")
+chat_history = [SystemMessage(content="You are a helpful AI assistant")]
 
-    if user_input == "exit":
-        break
-    result = model.invoke(user_input)
-    chat_history.append({"User": user_input, "AI": result.content})
-    print("AI: ", result.content)
+# while True:
+#     user_input = input("You:  ")
+#     chat_history.append(HumanMessage(content=user_input))
+#     if user_input == "exit":
+#         break
+#     result = model.invoke(user_input)
+#     chat_history.append(AIMessage(content=result.content))
+#     print("AI: ", result.content)
 
 print(chat_history)
